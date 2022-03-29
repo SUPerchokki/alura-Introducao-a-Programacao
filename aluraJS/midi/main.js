44ee3ac26cdf91e32f9a509bf36f3fc2c2bdede2
SUPerchokki <32721115+SUPerchokki@users.noreply.github.com>
2022-03-29 12:35:30 -0300

    criação das variaveis, primeira versão funcional

--- aluraJS/midi/main.js
+++ aluraJS/midi/main.js
@@ -9,7 +9,14 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 let contador = 0;
 
 while(contador < 9){
-    listaDeTeclas[contador].onclick = tocaSom;
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
 
     contador++;
 }
