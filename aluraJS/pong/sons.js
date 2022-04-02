954116fb84249cd6f0484480fb95cd96291db756
SUPerchokki <32721115+SUPerchokki@users.noreply.github.com>
2022-04-02 19:05:45 -0300

    correções de bugs

--- aluraJS/pong/sons.js
+++ aluraJS/pong/sons.js
@@ -6,6 +6,7 @@ let trilha;
 
 // Função para carregar os audios
 function preload(){
+    print("passou aqui")
     trilha = loadSound("trilha.mp3");
     ponto = loadSound("ponto.mp3");
     raquetada = loadSound("raquetada.mp3")
